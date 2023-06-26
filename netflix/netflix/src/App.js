88379ae0ad38fe9eb928c38a3a9e507a2874e883
fyxicek <fyxicek@gmail.com>
2023-06-26 16:44:58 +0200

    pokrok

--- netflix/netflix/src/App.js
+++ netflix/netflix/src/App.js
@@ -1,7 +1,19 @@
-const App = () => { 
-  return(
-    <button>efds</button>
-  )
+import data from "../data"
+
+
+const App = () => {
+  {
+    return <div>
+    data.map((oneMovie) => {
+    const {id, image, title, age, tags, description} = oneMovie
+
+    return<div key={id}>
+    <p>{title}</p>
+    </div>
+    
+    })
+  }
+  </div>
  }
 
  export default App
